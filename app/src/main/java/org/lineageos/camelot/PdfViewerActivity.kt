dc055eb03e1dc8842263512298a508d6a9f471cc
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-12-03 20:27:10 +0100

    Camelot: Load PDF

--- app/src/main/java/org/lineageos/camelot/PdfViewerActivity.kt
+++ app/src/main/java/org/lineageos/camelot/PdfViewerActivity.kt
@@ -5,21 +5,50 @@
 
 package org.lineageos.camelot
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.util.Consumer
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.pdf.viewer.fragment.PdfViewerFragment
 
-class PdfViewerActivity : AppCompatActivity() {
+class PdfViewerActivity : AppCompatActivity(R.layout.activity_main) {
+    // Fragment
+    @SuppressLint("NewApi")
+    private val pdfViewerFragment = PdfViewerFragment()
+
+    // Intents
+    private val intentListener = Consumer<Intent> { intent ->
+        intent.data?.let {
+            @SuppressLint("NewApi")
+            pdfViewerFragment.documentUri = it
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, pdfViewerFragment)
+            .commit()
+
+        addOnNewIntentListener(intentListener)
+        intentListener.accept(intent)
+    }
+
+    override fun onDestroy() {
+        removeOnNewIntentListener(intentListener)
+
+        super.onDestroy()
     }
 }

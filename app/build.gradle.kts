7d4bf50e6380dddf0688b78fcb870621f76c692b
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-12-04 00:44:02 +0100

    Camelot: Reorganize Gradle version catalog
    Change-Id: I710d92ac9bbcb89466872346f6e35adb1945b333

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,12 +69,11 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.pdf.viewer.fragment)
+    implementation(libs.material)
 }
 
 configure<GenerateBpPluginExtension> {

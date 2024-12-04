adc27e820fef165ebad12c31d720870f48a3cf48
LuK1337 <priv.luk@gmail.com>
2024-12-04 14:20:47 +0100

    Camelot: Use filename for print job name if available
    Change-Id: I43ac055f7af8f5c84d2df457aa77f30a32c2c937

--- app/src/main/java/org/lineageos/camelot/PdfViewerActivity.kt
+++ app/src/main/java/org/lineageos/camelot/PdfViewerActivity.kt
@@ -133,9 +133,14 @@ class PdfViewerActivity : AppCompatActivity(R.layout.activity_main) {
         R.id.action_print -> {
             pdfUri?.let {
                 val printManager = getSystemService(PrintManager::class.java)
-                val printDocumentAdapter = CamelotPdfDocumentAdapter(contentResolver, it)
+
+                val printDocumentAdapter = CamelotPdfDocumentAdapter(
+                    contentResolver,
+                    it,
+                    pdfViewModel.pdfName.value ?: getString(R.string.pdf_document)
+                )
                 printManager.print(
-                    "PDF Document",
+                    printDocumentAdapter.name,
                     printDocumentAdapter,
                     PrintAttributes.Builder().build()
                 )

23a45c4b2198954e2f3caf88297f223516fa2456
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-12-05 13:30:20 +0100

    [WIP] Camelot: Immersive mode
    Change-Id: I95f926f40f00a0f1589ae650ac5aae1aa456d69d

--- app/src/main/java/org/lineageos/camelot/fragments/CamelotPdfViewerFragment.kt
+++ app/src/main/java/org/lineageos/camelot/fragments/CamelotPdfViewerFragment.kt
@@ -7,13 +7,17 @@ package org.lineageos.camelot.fragments
 
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.annotation.RequiresExtension
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.activityViewModels
 import androidx.pdf.viewer.fragment.PdfViewerFragment
 import androidx.pdf.viewer.fragment.pdfName
+import org.lineageos.camelot.R
 import org.lineageos.camelot.ext.updateMargin
 import org.lineageos.camelot.viewmodels.PdfViewModel
 
@@ -21,11 +25,24 @@ import org.lineageos.camelot.viewmodels.PdfViewModel
 class CamelotPdfViewerFragment : PdfViewerFragment() {
     private val pdfViewModel by activityViewModels<PdfViewModel>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = super.onCreateView(inflater, container, savedInstanceState) as FrameLayout
+        val pdfView = inflater.inflate(R.layout.fragment_pdf, container, false) as FrameLayout
+
+        pdfView.addView(view)
+
+        return pdfView
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         sequenceOf<View>(
-            view.findViewById(androidx.pdf.R.id.fast_scroll_view),
+            view.findViewById(androidx.pdf.R.id.pdf_page_num),
             view.findViewById(androidx.pdf.R.id.edit_fab),
         ).forEach {
             ViewCompat.setOnApplyWindowInsetsListener(it) { v, windowInsets ->
@@ -33,6 +50,7 @@ class CamelotPdfViewerFragment : PdfViewerFragment() {
 
                 v.updateMargin(
                     insets,
+                    top = true,
                     bottom = true,
                 )
 
@@ -51,5 +69,11 @@ class CamelotPdfViewerFragment : PdfViewerFragment() {
         super.onLoadDocumentError(error)
 
         pdfViewModel.setPdfName(null)
+    }
+
+    override fun onRequestImmersiveMode(enterImmersive: Boolean) {
+        super.onRequestImmersiveMode(enterImmersive)
+
+        pdfViewModel.setImmersiveMode(enterImmersive)
     }
 }

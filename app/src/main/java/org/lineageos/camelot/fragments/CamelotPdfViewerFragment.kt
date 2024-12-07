96a30e6313c6bd89f6a331a2eec2a964a03dccea
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-12-07 21:59:55 +0100

    Camelot: Apply insets to fragment directly
    In 3-button navbar applying the inset within
    CamelotPdfViewerFragment doesn't work, while applying
    it in the FragmentContainerView does.
    
    Don't ask me why.
    
    Change-Id: I1f79c44e3e96d61cb8e91b45dd22e820a4132134

--- app/src/main/java/org/lineageos/camelot/fragments/CamelotPdfViewerFragment.kt
+++ app/src/main/java/org/lineageos/camelot/fragments/CamelotPdfViewerFragment.kt
@@ -6,40 +6,15 @@
 package org.lineageos.camelot.fragments
 
 import android.os.Build
-import android.os.Bundle
-import android.view.View
 import androidx.annotation.RequiresExtension
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.activityViewModels
 import androidx.pdf.viewer.fragment.PdfViewerFragment
 import androidx.pdf.viewer.fragment.pdfName
-import org.lineageos.camelot.ext.updateMargin
 import org.lineageos.camelot.viewmodels.PdfViewModel
 
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 13)
 class CamelotPdfViewerFragment : PdfViewerFragment() {
     private val pdfViewModel by activityViewModels<PdfViewModel>()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        sequenceOf<View>(
-            view.findViewById(androidx.pdf.R.id.fast_scroll_view),
-            view.findViewById(androidx.pdf.R.id.edit_fab),
-        ).forEach {
-            ViewCompat.setOnApplyWindowInsetsListener(it) { v, windowInsets ->
-                val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
-
-                v.updateMargin(
-                    insets,
-                    bottom = true,
-                )
-
-                windowInsets
-            }
-        }
-    }
 
     override fun onLoadDocumentSuccess() {
         super.onLoadDocumentSuccess()

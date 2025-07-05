1f3eae5a3882bdd15d7e2410d98511f6475459a9
LuK1337 <priv.luk@gmail.com>
2025-07-05 10:29:57 +0200

    Camelot: Update androidx.pdf to 1.0.0-alpha09
    Change-Id: I2e9b29d0e961822a94808ea3bce17089106bc656

--- app/src/main/java/org/lineageos/camelot/fragments/CamelotPdfViewerFragment.kt
+++ app/src/main/java/org/lineageos/camelot/fragments/CamelotPdfViewerFragment.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2024 The LineageOS Project
+ * SPDX-FileCopyrightText: 2024-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -20,11 +20,6 @@ import androidx.core.view.marginTop
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.activityViewModels
 import androidx.pdf.viewer.fragment.PdfViewerFragment
-import androidx.pdf.viewer.fragment.pdfName
-import androidx.pdf.widget.FastScrollView
-import androidx.pdf.widget.pageIndicatorExt
-import androidx.pdf.widget.zoomView
-import androidx.pdf.widget.zoomViewBasePadding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.lineageos.camelot.viewmodels.PdfViewModel
 
@@ -34,41 +29,6 @@ class CamelotPdfViewerFragment : PdfViewerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        ViewCompat.setOnApplyWindowInsetsListener(
-            view.findViewById(androidx.pdf.R.id.fast_scroll_view)
-        ) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
-
-            if (!pdfViewModel.immersiveMode.value) {
-                @Suppress("RestrictedApi")
-                (v as FastScrollView).apply {
-                    /**
-                     * @see FastScrollView.onApplyWindowInsets
-                     */
-                    zoomView?.also { zoomView ->
-                        zoomViewBasePadding.let {
-                            zoomView.setPadding(
-                                0,
-                                it.top + insets.top + pdfViewModel.toolbarHeight.value,
-                                0,
-                                it.bottom + insets.bottom,
-                            )
-                        }
-
-                        setScrollbarMarginTop(zoomView.paddingTop)
-                        setVerticalThumbMarginRight(insets.right)
-                        setScrollbarMarginBottom(zoomView.paddingBottom)
-                    }
-
-                    pageIndicatorExt.view.translationX = (-insets.right).toFloat()
-
-                    forceLayout()
-                }
-            }
-
-            windowInsets
-        }
 
         view.findViewById<FloatingActionButton>(androidx.pdf.R.id.edit_fab).let {
             val originalMargins = Rect(
@@ -95,7 +55,7 @@ class CamelotPdfViewerFragment : PdfViewerFragment() {
     override fun onLoadDocumentSuccess() {
         super.onLoadDocumentSuccess()
 
-        pdfViewModel.setPdfName(pdfName)
+        pdfViewModel.setPdfName(documentUri?.lastPathSegment)
     }
 
     override fun onLoadDocumentError(error: Throwable) {

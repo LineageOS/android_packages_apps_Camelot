ae18230b902a7e3467c6d338bad0d95317cf2b5b
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-09-21 10:53:51 +0200

    Camelot: Update SDK to 36
    Change-Id: I8d074a3ff109e2774d7fd73e512041d604b6d52d

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,12 @@ plugins {
 
 android {
     namespace = "org.lineageos.camelot"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "org.lineageos.camelot"
         minSdk = 35
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
